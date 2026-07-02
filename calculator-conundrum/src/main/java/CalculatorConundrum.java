2c0a3d5f4e26e1027f3b3d0ec5fb7824b1a402e0
Yoga Harisistianugroho <yogahariss@gmail.com>
2026-07-02 23:03:54 +0700

    add calculator-conundrum

---
+++ calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -0,0 +1,26 @@
+class CalculatorConundrum {
+    public String calculate(int operand1, int operand2, String operation) {
+        int result;
+        try {
+            return switch (operation){
+                case "+" -> {
+                    result = operand1 + operand2;
+                    yield operand1 + " " + operation + " " + operand2 + " = " + result;
+                }
+                case "*" -> {
+                    result = operand1 * operand2;
+                    yield operand1 + " " + operation + " " + operand2 + " = " + result;
+                }
+                case "/" -> {
+                    result = operand1 / operand2;
+                    yield operand1 + " " + operation + " " + operand2 + " = " + result;
+                }
+                case null -> throw new IllegalArgumentException("Operation cannot be null");
+                case "" -> throw new IllegalArgumentException("Operation cannot be empty");
+                default -> throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+            };
+        } catch (ArithmeticException e){
+            throw new IllegalOperationException("Division by zero is not allowed", e);
+        }
+    }
+}
